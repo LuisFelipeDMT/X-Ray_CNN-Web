fead604b7311fb009bbe8848ef0b5be9ba1c2e42
LuisFelipeDMT <93631443+LuisFelipeDMT@users.noreply.github.com>
2022-05-31 14:26:09 -0300

    Update settings.py

--- webapp_xrimage/webapp_xrimage/settings.py
+++ webapp_xrimage/webapp_xrimage/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-5z&u$*p=@wh6s!67w)j(!wp9_24ekoo*#x3rnb*cqirfp#a+wh'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
