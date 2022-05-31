e13276f53464de4ba976336d38ddae38e8fc75df
LuisFelipeDMT <luisfelipe_trindade@hotmail.com>
2022-05-31 20:46:37 -0300

    Inclusao de STATICFILES_DIRS

--- xrimage/settings.py
+++ xrimage/settings.py
@@ -132,4 +132,9 @@ STATIC_ROOT= os.path.join(BASE_DIR,'staticfiles')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
 django_heroku.settings(locals())
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
