e84189c9ffe932dfffc2f559d8df7f26cda32b49
LuisFelipeDMT <luisfelipe_trindade@hotmail.com>
2022-06-01 00:36:40 -0300

    incluindo staticfiles do firebase

--- xrimage/settings.py
+++ xrimage/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
-SECRET_KEY = config(SECRET_KEY)
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = FALSE
@@ -128,10 +128,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "home/static"),
-)
 
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
