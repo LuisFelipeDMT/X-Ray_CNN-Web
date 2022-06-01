e1c7bf56bea290119034f7aed9d6dee234d8141b
LuisFelipeDMT <luisfelipe_trindade@hotmail.com>
2022-06-01 00:15:01 -0300

    Update Django for whitenoise static

--- xrimage/settings.py
+++ xrimage/settings.py
@@ -15,6 +15,7 @@ from pickle import FALSE
 import django_heroku
 from decouple import config
 import dj_database_url
+import os
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -127,6 +128,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "home/static"),
+)
+
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
