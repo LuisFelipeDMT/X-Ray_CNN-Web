294c4a751c1c7e94c79ddee056d76294ba6ee5e8
LuisFelipeDMT <luisfelipe_trindade@hotmail.com>
2022-06-02 08:26:06 -0300

    site funcional integrado à API

--- xrimage/settings.py
+++ xrimage/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = config('SECRET_KEY')
 
-DEBUG = config('DEBUG', cast=bool)
+DEBUG = False
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
